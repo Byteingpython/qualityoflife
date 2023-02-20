eb6e93808f9ba1e47787d3b405941a7cac025fa2
Florian Ippisch <maximco@freenet.de>
2023-02-20 15:08:04 +0100

    First Commit

---
+++ src/main/java/de/snowwars/qualityoflife/Qualityoflife.java
@@ -0,0 +1,17 @@
+package de.snowwars.qualityoflife;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Qualityoflife extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
