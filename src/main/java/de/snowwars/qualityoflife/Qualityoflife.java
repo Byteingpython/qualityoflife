aaa53f688e360b06ea3bdc46c01eff3d48cbc55e
Byteingpython <byteingpython@freenet.de>
2023-02-23 17:46:12 +0100

    Save Inventory on Death

--- src/main/java/de/snowwars/qualityoflife/Qualityoflife.java
+++ src/main/java/de/snowwars/qualityoflife/Qualityoflife.java
@@ -1,8 +1,12 @@
 package de.snowwars.qualityoflife;
 
 import de.snowwars.qualityoflife.keepinventory.KeepImportant;
+import de.snowwars.qualityoflife.keepinventory.SaveInventory;
 import de.snowwars.qualityoflife.shulker.ShulkerListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public final class Qualityoflife extends JavaPlugin {
 
@@ -12,7 +16,7 @@ public final class Qualityoflife extends JavaPlugin {
         InventoryListener inventoryListener=new InventoryListener(this);
         ShulkerListener shulkerListener=new ShulkerListener(this);
         KeepImportant keepImportant=new KeepImportant(this);
-
+        SaveInventory saveInventory=new SaveInventory(this);
     }
 
     @Override
