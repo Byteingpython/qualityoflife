a1852f5f832a19c1de1ac8cd58ac32acf50ba1ef
Byteingpython <byteingpython@freenet.de>
2023-02-25 18:33:13 +0100

    Status Indicators

--- src/main/java/de/snowwars/qualityoflife/Qualityoflife.java
+++ src/main/java/de/snowwars/qualityoflife/Qualityoflife.java
@@ -3,7 +3,10 @@ package de.snowwars.qualityoflife;
 import de.snowwars.qualityoflife.keepinventory.KeepImportant;
 import de.snowwars.qualityoflife.keepinventory.SaveInventory;
 import de.snowwars.qualityoflife.shulker.ShulkerListener;
+import de.snowwars.qualityoflife.status.StatusCommand;
+import de.snowwars.qualityoflife.status.StatusManager;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
@@ -13,10 +16,17 @@ public final class Qualityoflife extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        FileConfiguration configuration = getConfig();
+        saveDefaultConfig();
         InventoryListener inventoryListener=new InventoryListener(this);
         ShulkerListener shulkerListener=new ShulkerListener(this);
         KeepImportant keepImportant=new KeepImportant(this);
         SaveInventory saveInventory=new SaveInventory(this);
+        StatusCommand statusCommand = new StatusCommand(configuration, this);
+        this.getCommand("status").setExecutor(statusCommand);
+        this.getCommand("status").setTabCompleter(statusCommand);
+
+
     }
 
     @Override
---
+++ src/main/java/de/snowwars/qualityoflife/status/StatusCommand.java
@@ -0,0 +1,62 @@
+package de.snowwars.qualityoflife.status;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import static org.bukkit.util.StringUtil.copyPartialMatches;
+
+public class StatusCommand implements TabExecutor {
+    FileConfiguration configuration;
+    Plugin plugin;
+    StatusManager statusManager;
+
+    public StatusCommand(FileConfiguration configuration, Plugin plugin) {
+        this.configuration = configuration;
+        this.plugin = plugin;
+        this.statusManager = new StatusManager(configuration, plugin);
+        plugin.reloadConfig();
+        this.configuration= plugin.getConfig();
+    }
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(args.length <= 1){
+            return false;
+        }
+        if(!(sender instanceof Player)){
+            return false;
+        }
+        if(statusManager.setStatus(args[0], args[1], ((Player) sender).getUniqueId())){
+            sender.sendMessage("Status set");
+        }
+        else{
+            sender.sendMessage("Invalid status");
+        }
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        List<String> completions = new ArrayList<>();
+        if(args.length== 1){
+            completions.addAll(configuration.getConfigurationSection("stati.first").getKeys(false));
+        }
+        if(args.length== 2){
+            completions.addAll(configuration.getConfigurationSection("stati.second").getKeys(false));
+        }
+        List<String> matches = new ArrayList<>();
+        copyPartialMatches(args[args.length-1], completions, matches);
+        return matches;
+    }
+}
